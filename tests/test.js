46a331415e5cdef4c071b41495b8d00fad42d24a
Armando Gutierrez <agutierr@adobe.com>
2017-03-24 21:54:32 +0100

    Tabs to spaces

--- tests/test.js
+++ tests/test.js
@@ -4,11 +4,11 @@ describe('Processing the arguments', function() {
     it('has default arguments', function() {
         var clock = new BetterClock();
         expect (clock.args).to.equal({
-				longitude: 0,
-				latitude: 0,
-				wakeupAt: 630,
-				sleepAt: 2230
-			});
+                longitude: 0,
+                latitude: 0,
+                wakeupAt: 630,
+                sleepAt: 2230
+            });
     });
 
     it('fails if longitude > 90', function() {

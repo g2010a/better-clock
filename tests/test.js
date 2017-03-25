bfc2b5c753b62a7b150ec8d16023b8e3b4a00fa7
g2010a <g2010a@mailinater.com>
2017-03-25 20:04:21 +0100

    Test failure if longitude outside normal parameters

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,30 @@
 var expect = chai.expect;
 
 describe('Processing the arguments', function() {
-    it('has default arguments', function() {
-        var clock = new BetterClock();
-        expect (clock.args).to.equal({
-                longitude: 0,
-                latitude: 0,
-                wakeupAt: 630,
-                sleepAt: 2230
-            });
+    var sandbox;
+
+    beforeEach(function(){
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(Logger, "useDefaults");
+        sandbox.stub(Logger, "debug");
+        sandbox.stub(Logger, "info");
     });
 
-    it('fails if longitude > 90', function() {
-        expect (var clock = new BetterClock({longitude: 91});)
+    afterEach(function(){
+        sandbox.restore();
     });
+
+    it('has default arguments', function() {
+        var clock = new BetterClock({});
+        expect(clock.args).to.have.property('longitude', 0);
+        expect(clock.args).to.have.property('latitude', 0);
+        expect(clock.args).to.have.property('wakeupAt', 630);
+        expect(clock.args).to.have.property('sleepAt', 2230);
+    });
+
+    it('fails if longitude is not within -90 <-> +90', function() {
+        expect(() => new BetterClock({ longitude: 91 })).to.throw('Longitude exceeds limits [-90,90]');
+        expect(() => new BetterClock({ longitude: -91 })).to.throw('Longitude exceeds limits [-90,90]');
+    });
+
 });

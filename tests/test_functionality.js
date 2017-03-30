a2f1fdf7b72c6c4310cdd3481f2c45d2ad70d83d
g2010a <g2010a@mailinater.com>
2017-03-30 21:41:06 +0200

    Test data set correctly

--- tests/test_functionality.js
+++ tests/test_functionality.js
@@ -22,17 +22,12 @@ describe('Functionality', function() {
         expect(clock.args).to.have.property('sleepAt', 2230);
     });
 
-    it('sets milestones correctly', function() {
-        let timeMachine = sinon.useFakeTimers(new Date(1999, 1, 1, 8, 0, 0, 0).getTime());
-        let clock = new BetterClock({});
-        expect(clock.milestones.dayStart).to.deep.equal(moment(new Date('1999-02-01T06:11:19.242Z')));
-        expect(clock.milestones.sunsetStart).to.deep.equal(moment(new Date('1999-02-01T18:18:17.662Z')));
-        expect(clock.milestones.nightStart).to.deep.equal(moment(new Date('1999-02-01T19:30:16.568Z')));
-        expect(clock.milestones.dawnStart).to.deep.equal(moment(new Date('1999-02-01T05:49:41.282Z')));
-        expect(clock.milestones.wakeupAt).to.deep.equal(clock.args.wakeupAt);
-        expect(clock.milestones.sleepAt).to.deep.equal(clock.args.sleepAt);
-
-        timeMachine.restore();
+    it('sets clock data correctly', function() {
+        let clock = new BetterClock({date: '1999-01-01'});
+        expect(clock.data[0].start).to.deep.equal(moment(new Date('1998-12-31T05:38:02.884Z')));
+        expect(clock.data[1].start).to.deep.equal(moment(new Date('1998-12-31T06:00:31.482Z')));
+        expect(clock.data[2].start).to.deep.equal(moment(new Date('1998-12-31T18:07:46.146Z')));
+        expect(clock.data[3].start).to.deep.equal(moment(new Date('1998-12-31T19:22:39.780Z')));
     });
 
 });
